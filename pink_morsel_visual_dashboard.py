4e15aaeccf11ebb81270696a773728c384194bd2
niaanderson-po <niaanderson.po@gmail.com>
2025-02-20 17:51:30 -0500

    refactor: isolate app layout and app components header, visual , and filter for easier maintenance and dynamic arrangement

--- pink_morsel_visual_dashboard.py
+++ pink_morsel_visual_dashboard.py
@@ -1,9 +1,13 @@
 from dash import Dash, dcc, html, Input, Output
-import plotly.express as px
+from plotly.express import line
 import pandas as pd
 
 #path to the formatted data file 
 DATA_PATH = "./formatted_data.csv"
+COLORS = {
+    "primary":"black",
+    "secondary": "#e5ecf6",
+}
 
 #load in data
 df = pd.read_csv(DATA_PATH)
@@ -11,48 +15,74 @@ df = pd.read_csv(DATA_PATH)
 #initialize the app
 app = Dash(__name__)
 
-#app layout with html and css
-app.layout = html.Div([
+# create the #graph representation of df - dataframe
+def generate_figure(chart_data):
+    line_chart = line(chart_data, x="date", y="sales")
+    return line_chart
 
-    #centered header
-    html.Header(
-        "Pink Morsel Sales Over Time",
-        style={"font-size": "30px", "width": "400px", "textAlign": "center", "margin": "auto", "background":"#e5ecf6"}
-    ),
-    
-    #center-left div
-    html.Div([
+visualization = dcc.Graph(
+    id="visualization",
+    figure=generate_figure(df)
+)
+
+#centered header
+header = html.H1(
+    "Pink Morsel Sales Over Time",
+    id="header",
+    style={
+        "font-size": "30px", 
+        "width": "400px", 
+        "textAlign": "center", 
+        "margin": "auto", 
+        "background-color": COLORS["secondary"],
+    }
+)
+
+#centered left region filter
+region_picker = dcc.RadioItems(
+    options = ["all", "north", "south", "east", "west"],
+    value = "all",
+    id="radio",
+    inline=True,
+)
+
+region_picker_wrapper = html.Div(
+    [
         html.P("Select Region:", style={"font-size": "15px"}),
-        dcc.RadioItems(
-            id="radio",
-            options=[ "all", "north", "south", "east", "west"],
-            value="all",
-            inline=True,
-        )
-    ], style={"display": "flex", "alignItems": "center", "width": "400px", "margin": "auto"}),
+        region_picker
+    ],
+    style={
+        "display": "flex", 
+        "alignItems": "center", 
+        "width": "400px", 
+        "margin": "auto"
+    }
+)
 
-    #graph representation of df - dataframe
-    dcc.Graph(figure={}, id="graph")
-
-], style={"margin": 10, "maxWidth": 800})
-
-#added controls/input-output for user interaction
+#define input-output for user interaction
 @app.callback( 
-    Output("graph", "figure"),
-    Input("radio", "value")
+    Output(visualization, "figure"),
+    Input(region_picker, "value")
     )
 
 #callback function
-def update_line_chart(region):
+def update_grapgh(region):
     #identitfy specific data related to region(s) selected
     if region == "all":
         trimmed_data = df
     else:
         trimmed_data = df[df["region"] == region]
 
-    #build and return graph/figure based on requested data
-    fig = px.line(trimmed_data, x="date", y="sales", color='region')
+    # generate a new line chart with the filtered data
+    fig = generate_figure(trimmed_data)
     return fig
+
+#app layout with html and css
+app.layout = html.Div([
+    header,
+    region_picker_wrapper,
+    visualization
+], style={"margin": 10, "maxWidth": 800})
 
 #run the app
 if __name__ == '__main__':
